02264839f38550f8d3393977eb4335d978155a4e
AutoReiv <auopak8@gmail.com>
2024-09-03 14:14:52 -0400

    feat: Add API resources endpoint

--- pkg/handlers/rbac/apiresources.go
+++ pkg/handlers/rbac/apiresources.go
@@ -8,15 +8,20 @@ import (
 )
 
 // APIResourcesHandler handles retrieving all Kubernetes API resources.
+// It uses the Kubernetes clientset to list the available API resources and returns them as a JSON response.
 func APIResourcesHandler(clientset *kubernetes.Clientset) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Create a discovery client to list available API resources
 		discoveryClient := clientset.Discovery()
+
+		// Retrieve the list of preferred API resources
 		apiResources, err := discoveryClient.ServerPreferredResources()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// Collect the names of the API resources
 		var resources []string
 		for _, resourceList := range apiResources {
 			for _, resource := range resourceList.APIResources {
@@ -24,6 +29,7 @@ func APIResourcesHandler(clientset *kubernetes.Clientset) http.HandlerFunc {
 			}
 		}
 
+		// Write the list of API resources as a JSON response
 		utils.WriteJSON(w, map[string][]string{"resources": resources})
 	}
 }
