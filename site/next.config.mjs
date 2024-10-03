964906b68103aa2c9e040c70d8f08921bd93afa1
Omar Baig <obaig992@gmail.com>
2024-10-03 19:24:54 -0400

    redirects

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,4 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard/roles",
+        permanent: true,
+      },
+      {
+        source: "/dashboard",
+        destination: "/dashboard/roles",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
